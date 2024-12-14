4bc4104865bf2840ed5050aa7e3f12ceb9805de4
TiagoM13 <thyagomotha2000@gmail.com>
2024-12-14 15:24:46 -0300

    feat: add pagination and get status text

--- src/app/views/clients/clients.component.ts
+++ src/app/views/clients/clients.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { clients } from 'src/app/data/clients';
+import { Client, Status } from './clients.model';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-client',
@@ -7,11 +10,27 @@ import { clients } from 'src/app/data/clients';
   styleUrls: ['./clients.component.scss']
 })
 export class ClientComponent implements OnInit {
-  dataClients = clients
   displayedColumns: string[] = ["inscricao", "apelido", "nome", "status"]
+  dataClients = new MatTableDataSource<Client>(clients)
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataClients.paginator = this.paginator
+  }
+
+  getStatusText(status: Status): string {
+    switch (status) {
+      case Status.DESATIVADO:
+        return 'DESATIVADO';
+      case Status.ATIVO:
+        return 'ATIVO';
+      case Status.SUSPENSO:
+        return 'SUSPENSO';
+      default:
+        return '';
+    }
   }
 }
